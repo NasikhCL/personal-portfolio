7bd0e7146490045847dc5e7958c0f177ad1edf2a
NasikhCL <nasikcl@gmail.com>
2022-11-05 23:42:34 +0530

    first commit: nav bar and partial header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Header (){
+    return(
+        <div className="header-container">
+            <div className="header-left">
+                <div className="header-btn"><h5>Front-End Developer</h5></div>
+                <h1 className="header-name">Hi,</h1>
+                <h1 className="header-name">I'm Nasikh CL</h1>
+            </div>
+        
+            <img className="main-image" src=".nasikh.png" alt="portfoliophoto" />
+           
+        
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <div className="navbar-container">
+            <div className="logo">
+                <h2>Nasikh CL</h2>
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Portfolio</li>
+            </ul>
+        </div>
+    )
+}
