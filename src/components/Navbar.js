b3d5e1f591d196a019a210054a4b306359c76fc3
NasikhCL <nasikcl@gmail.com>
2022-11-07 01:12:40 +0530

    added media tag for header and nav components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 export default function Navbar(){
+    const [isToggled, setIsToggled] = useState(false)
+
+    function toggle(){
+        setIsToggled(prevState=> !prevState);
+
+    }
+
     return(
         <div className="navbar-container">
             <div className="logo">
@@ -12,6 +21,17 @@ export default function Navbar(){
                 <li>Skills</li>
                 <li>Portfolio</li>
             </ul>
+            
+            <div className="hamberger" onClick={toggle}><img src="https://img.icons8.com/external-febrian-hidayat-gradient-febrian-hidayat/45/000000/external-hamburger-ui-essential-febrian-hidayat-gradient-febrian-hidayat.png"/></div>
+            {isToggled && <div className="hamberger-items-contaner">
+            <img onClick={toggle} src="https://img.icons8.com/ios-glyphs/48/737373/delete-sign.png"/>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Skills</li>
+                    <li>Portfolio</li>
+                </ul>
+            </div>}
         </div>
     )
 }
