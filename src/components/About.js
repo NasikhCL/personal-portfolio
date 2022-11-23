a2aebbc6e1ded5350f3a3558b324de2420679ea4
Nasikh CL <nasikcl@gmail.com>
2022-11-23 23:42:03 +0530

    minor change in about section: about me content edited

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,7 @@ export default function About(){
             <h1>About Me</h1>
             <div className="about">
                 <p>
-                    I’m a React developer based in Kerala, India. curious to learn more about both frontend as well as backend technologies. Currently working as a part-time intern as teaching assistant in web deveopement. solved 600+ doutes of students, helping them in projects and other assignments with an average rating of 4.87/5.
-                    If I'm not coding, you can find me at badminton court.
+                I'm a React developer based in Kerala, India. curious to learn more about both frontend and backend technologies. Currently working as a part-time intern as a web development teaching assistant. solved 600+ student doubts, assisting them with projects and other assignments with an average rating of 4.87/5. If I'm not coding, you can find me at the badminton court.
                 </p>
             </div>
         </div>
