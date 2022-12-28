a93154667a8b786b8ed6626d4db3b3ff92455954
Nasikh CL <nasikcl@gmail.com>
2022-12-28 23:05:26 +0530

    footer minor change

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ export default function About(){
             <h1>About Me</h1>
             <div className="about">
                 <p>
-                I'm a React developer based in Kerala, India. curious to learn more about both frontend and backend technologies. Currently working as a part-time intern as a web development teaching assistant. solved 600+ student doubts, assisting them with projects and other assignments with an average rating of 4.87/5. If I'm not coding, you can find me at the badminton court.
+                I'm a React developer based in Kerala, India. curious to learn more about both frontend and backend technologies. Worked as a part-time intern as a web development teaching assistant. solved 760+ student doubts, assisting them with projects and other assignments with an average rating of 4.8/5. If I'm not coding, you can find me at the badminton court.
                 </p>
             </div>
         </div>
