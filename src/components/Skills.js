6eabae42c545b903d2de236ee244d4cd19dab9b4
Nasikh CL <nasikcl@gmail.com>
2022-12-28 21:17:03 +0530

    skills section updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,13 +6,45 @@ export default function Skills(){
 
         <div  id='skills-section'  className="section skills">
             <h1>Skills</h1>
-            <div className="skills-img-container"></div>
-            <img src="https://img.icons8.com/color/100/null/html-5--v1.png"/>
-            <img src="https://img.icons8.com/color/100/null/css3.png"/> 
-            <img src="https://img.icons8.com/color/100/null/javascript--v1.png"/>
-            <img src="https://img.icons8.com/plasticine/100/null/react.png"/>
-            <img src="https://img.icons8.com/color/100/null/git.png"/>
-
+            <div className="skills-img-container">
+                <div className="skill-box">
+                    <h4>HTML</h4>
+                    <img src="https://img.icons8.com/color/100/null/html-5--v1.png"/>
+                </div>
+                <div className="skill-box">
+                    <h4>CSS</h4>
+                    <img src="https://img.icons8.com/color/100/null/css3.png"/> 
+                </div>
+                <div className="skill-box">
+                    <h4>JAVASCRIPT</h4>
+                    <img src="https://img.icons8.com/color/100/null/javascript--v1.png"/>
+                </div>
+                <div className="skill-box">
+                    <h4>REACT JS</h4>
+                    <img src="https://img.icons8.com/plasticine/100/null/react.png"/>
+                </div>
+                <div className="skill-box">
+                    <h4>NODE JS</h4>
+                    <img src="https://img.icons8.com/color/100/null/nodejs.png"/>
+                    <img src="https://img.icons8.com/fluency/40/null/node-js.png" style={{display: "block", margin:"-40px auto 0"}}/>
+                </div>
+                <div className="skill-box">
+                    <h4>EXPRESS JS</h4>
+                    <img src="https://img.icons8.com/ios/100/null/express-js.png" />
+                </div> 
+                <div className="skill-box">
+                    <h4>MONGO DB</h4>
+                    <img src="https://img.icons8.com/color/100/null/mongodb.png"/> 
+                </div> 
+                <div className="skill-box">
+                    <h4>FIREBASE</h4>
+                    <img src="https://img.icons8.com/color/100/null/firebase.png"/>
+                </div> 
+                <div className="skill-box">
+                    <h4>GIT</h4>
+                    <img src="https://img.icons8.com/color/100/null/git.png"/>
+                </div>    
+            </div>
             
         </div>
     )
